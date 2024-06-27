b8e4d473d381dcb521a0971e794533bfd8d4617a
Blattvorhang <hanyi.cn@outlook.com>
2024-06-27 19:00:39 +0800

    added main.cpp

---
+++ src/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+int main()
+{
+  try
+  {
+    std::cout << "Hello, World!\n" << std::endl;
+  }
+  catch (const std::exception& e)
+  {
+    std::cerr << "Caught exception:\n"
+              << e.what() << "\n";
+  }
+  catch (...)
+  {
+    std::cerr << "Caught unknown exception\n";
+  }
+
+  return EXIT_SUCCESS;
+}
