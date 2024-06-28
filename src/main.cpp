e3bad72458e8e85d166e0408a13b5fb74aa1dccc
Blattvorhang <hanyi.cn@outlook.com>
2024-06-28 17:45:58 +0800

    main arguments

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,59 @@
 #include <iostream>
+#include <string>
 #include "../include/stereo_system.h"
 
-int main()
+int main(int argc, char *argv[])
 {
+    /**
+     * Usage: [-c | --camera <camera_id>]
+     *        [-s | --size <width> <height>]
+     *        [-d | --debug]
+     *        [-h | --help]
+     */
+    const std::string param_path = "../calibration/";
+    int camera_id = 0;
+    int width = 1280;
+    int height = 720;
+    bool enable_debug = false;
+
+    if (argc > 1)
+    {
+        for (int i = 1; i < argc; ++i)
+        {
+            std::string arg = argv[i];
+            if (arg == "-c" || arg == "--camera")
+            {
+                if (i + 1 < argc)
+                {
+                    camera_id = std::stoi(argv[++i]);
+                }
+            }
+            else if (arg == "-s" || arg == "--size")
+            {
+                if (i + 2 < argc)
+                {
+                    width = std::stoi(argv[++i]);
+                    height = std::stoi(argv[++i]);
+                }
+            }
+            else if (arg == "-d" || arg == "--debug")
+            {
+                enable_debug = true;
+            }
+            else if (arg == "-h" || arg == "--help")
+            {
+                std::cout << "Usage: [-c | --camera <camera_id>]" << std::endl
+                          << "       [-s | --size <width> <height>]" << std::endl
+                          << "       [-d | --debug]" << std::endl
+                          << "       [-h | --help]" << std::endl;
+                return EXIT_SUCCESS;
+            }
+        }
+    }
+    
     try
     {
-        StereoSystem stereo_system;
+        StereoSystem stereo_system(param_path, camera_id, width, height, enable_debug);
         stereo_system.run();
     }
     catch (const std::exception &e)
--- src/stereo_system.cpp
+++ src/stereo_system.cpp
@@ -17,13 +17,13 @@ StereoSystem::StereoSystem(const std::string &param_path,
     readCalibrationParameters(param_path);
 }
 
-
-void StereoSystem::checkSize(const cv::Mat& mat, int expected_rows, int expected_cols) {
-    if (mat.rows != expected_rows || mat.cols != expected_cols) {
+void StereoSystem::checkSize(const cv::Mat &mat, int expected_rows, int expected_cols)
+{
+    if (mat.rows != expected_rows || mat.cols != expected_cols)
+    {
         throw std::runtime_error("Matrix size does not match the expected size.");
     }
 }
-
 
 void StereoSystem::readCalibrationParameters(const std::string &param_path)
 {
@@ -44,16 +44,18 @@ void StereoSystem::readCalibrationParameters(const std::string &param_path)
     fs["T"] >> T_;
 
     // Check the sizes of the matrices.
-    try {
+    try
+    {
         checkSize(cameraMatrixL, 3, 3); // Assuming camera matrices are 3x3
         checkSize(distCoeffsL, 1, 5);   // Assuming distortion coefficients are 1x5
-        checkSize(cameraMatrixR, 3, 3); 
-        checkSize(distCoeffsR, 1, 5); 
-        checkSize(R_, 3, 3);             // Rotation matrix is 3x3
-        checkSize(T_, 3, 1);             // Translation vector is 3x1
-    } catch (const std::runtime_error& e) {
-        std::cerr << "Error: " << e.what() << std::endl;
-        std::exit(EXIT_FAILURE);
+        checkSize(cameraMatrixR, 3, 3);
+        checkSize(distCoeffsR, 1, 5);
+        checkSize(R_, 3, 3); // Rotation matrix is 3x3
+        checkSize(T_, 3, 1); // Translation vector is 3x1
+    }
+    catch (const std::runtime_error &e)
+    {
+        throw std::runtime_error("Error reading calibration parameters: " + std::string(e.what()));
     }
 
     std::cout << "Camera matrices and distortion coefficients loaded successfully." << std::endl;
@@ -65,7 +67,6 @@ void StereoSystem::readCalibrationParameters(const std::string &param_path)
     fs.release();
 }
 
-
 void StereoSystem::calibrateStereoCameras()
 {
     cv::stereoRectify(left_camera_.getIntrinsicMatrix(), left_camera_.getDistortionCoeffs(),
@@ -75,9 +76,8 @@ void StereoSystem::calibrateStereoCameras()
                       cv::CALIB_ZERO_DISPARITY, 0, cv::Size(width_, height_));
 }
 
-
 void StereoSystem::rectifyImages(const cv::Mat &ori_left, const cv::Mat &ori_right,
-                   cv::Mat &rectified_ori_left, cv::Mat &rectified_ori_right)
+                                 cv::Mat &rectified_ori_left, cv::Mat &rectified_ori_right)
 {
     cv::Mat rect_map_L1, rect_map_L2, rect_map_R1, rect_map_R2;
 
@@ -91,7 +91,6 @@ void StereoSystem::rectifyImages(const cv::Mat &ori_left, const cv::Mat &ori_rig
     cv::remap(ori_left, rectified_ori_left, rect_map_L1, rect_map_L2, cv::INTER_LINEAR);
     cv::remap(ori_right, rectified_ori_right, rect_map_R1, rect_map_R2, cv::INTER_LINEAR);
 }
-
 
 void StereoSystem::captureImages(cv::VideoCapture &cap, cv::Mat &left_image, cv::Mat &right_image)
 {
@@ -114,8 +113,8 @@ void StereoSystem::run()
 
     calibrateStereoCameras();
 
-    cv::Mat ori_left, ori_right;    // original images
-    cv::Mat rect_left, rect_right;  // stereo rectified images
+    cv::Mat ori_left, ori_right;   // original images
+    cv::Mat rect_left, rect_right; // stereo rectified images
 
     while (true)
     {
@@ -128,80 +127,21 @@ void StereoSystem::run()
 
         // TODO: Compute disparity map.
 
-
         // Show additional debug/educational figures.
         if (enable_debug_)
         {
             // if (!frame.empty())      { cv::imshow("Original", frame); }
-            if (!ori_left.empty())  { cv::imshow("Original Left", ori_left); }
-            if (!ori_right.empty()) { cv::imshow("Original Right", ori_right); }
+            if (!ori_left.empty())
+            {
+                cv::imshow("Original Left", ori_left);
+            }
+            if (!ori_right.empty())
+            {
+                cv::imshow("Original Right", ori_right);
+            }
         }
 
         // Update the windows.
         cv::waitKey(1);
     }
-
-    /*
-    // Process each image in the dataset.
-    for (DataElement element{}; dataset >> element;)
-    {
-      // Todo 1: Convert geographical position and attitude to local Cartesian pose.
-      // Compute the pose of the body in the local coordinate system.
-      // DONE 1.1: Finish Attitude::toQuaternion().
-      const Sophus::SE3d pose_local_body = local_system.toLocalPose(element.body_position_in_geo,
-                                                                    element.body_attitude_in_geo.toSO3());
-
-      // Add body coordinate axis to the 3D viewer.
-      // DONE 1.2: Write line of code below to add body to viewer.
-      viewer.addBody(pose_local_body, element.img_num);
-
-      // Todo 2: Compute the pose of the camera
-      // Compute the pose of the camera relative to the body.
-      // DONE 2.1: Finish CartesianPosition::toVector().
-      // DONE 2.2: Construct pose_body_camera correctly using element.
-      const Sophus::SE3d pose_body_camera{element.camera_attitude_in_body.toSO3(),
-                                          element.camera_position_in_body.toVector()};
-
-      // Compute the pose of the camera relative to the local coordinate system.
-      // DONE 2.3: Construct pose_local_camera correctly using the poses above.
-      const Sophus::SE3d pose_local_camera{pose_local_body * pose_body_camera};
-
-      // Todo 3: Undistort the images.
-      // Construct a camera model based on the intrinsic calibration and camera pose.
-      // DONE 3.1: Finish Intrinsics::toCalibrationMatrix().
-      // DONE 3.2: Finish Intrinsics::toDistortionVector().
-      const PerspectiveCameraModel camera_model{element.intrinsics.toCalibrationMatrix(),
-                                               pose_local_camera,
-                                               element.intrinsics.toDistortionCoefficientVector()};
-
-      // Undistort image.
-      // DONE 3.3: Undistort image using the camera model. Why should we undistort the image?
-      // Undistort the original image, instead of using it directly.
-      cv::Mat undistorted_img = camera_model.undistortImage(element.image);
-
-      // Todo 4: Project a geographic world point into the images
-      // Project world point (the origin) into the image.
-      // DONE 4.1: Finish PerspectiveCameraModel::computeCameraProjectionMatrix().
-      // DONE 4.2: Finish PerspectiveCameraModel::projectWorldPoint().
-      // DONE 4.3: Optionally finish PerspectiveCameraModel::projectWorldPoints().
-      const Eigen::Vector2d pix_pos = camera_model.projectWorldPoint(Eigen::Vector3d::Zero());
-
-      // Draw a marker in the image at the projected position.
-      const Eigen::Vector2i pix_pos_int = (pix_pos.array().round()).cast<int>();
-      cv::drawMarker(undistorted_img, {pix_pos_int.x(), pix_pos_int.y()}, {0.,0.,255.}, cv::MARKER_CROSS, 40, 3);
-
-      // Show the image.
-      // DONE: Write line of code below to show the image with the marker.
-      cv::imshow(window_name_, undistorted_img);
-      // Add the camera to the 3D viewer.
-      // DONE 4.4: Write line of code below to add body to viewer.
-      viewer.addCamera(camera_model, undistorted_img, element.img_num);
-      // Update the windows.
-      viewer.spinOnce();
-      cv::waitKey(100);
-    }
-
-    // Remove image viewer.
-    cv::destroyWindow(window_name_);
-    */
 }
